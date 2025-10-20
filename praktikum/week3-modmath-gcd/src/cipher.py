6389b1bd79669ec828700f0fb4d2ecb73358bbf9
XavarcodeX <164608214+XavarcodeX@users.noreply.github.com>
2025-10-20 11:36:05 +0700

    Update cipher.py

--- praktikum/week3-modmath-gcd/src/cipher.py
+++ praktikum/week3-modmath-gcd/src/cipher.py
@@ -16,3 +16,19 @@ def gcd(a, b):
 
 print("gcd(54, 24) =", gcd(54, 24))
 
+#menentukan invers 
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    g, x1, y1 = egcd(b % a, a)
+    return g, y1 - (b // a) * x1, x1
+
+def modinv(a, n):
+    g, x, _ = egcd(a, n)
+    if g != 1:
+        return None
+    return x % n
+
+print("Invers 3 mod 11 =", modinv(3, 11))  # hasil: 4
+
+
