c962b1e581a23509c103dc2aa549b48d89c1d730
XavarcodeX <164608214+XavarcodeX@users.noreply.github.com>
2025-11-18 06:33:33 +0700

    Update cipher.py

--- praktikum/week3-modmath-gcd/src/cipher.py
+++ praktikum/week3-modmath-gcd/src/cipher.py
@@ -31,4 +31,12 @@ def modinv(a, n):
 
 print("Invers 3 mod 11 =", modinv(3, 11))  # hasil: 4
 
+#logaritma diskret
+def discrete_log(a, b, n):
+    for x in range(n):
+        if pow(a, x, n) == b:
+            return x
+    return None
+
+print("3^x ≡ 4 (mod 7), x =", discrete_log(3, 4, 7))  # hasil: 4
 
